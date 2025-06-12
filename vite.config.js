e56c46e60419d81cd312ec4929b70fe7129fa601
nageshmali <malinagesh60@gmail.com>
2025-06-12 15:55:08 +0530

    vite config file

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // 👈 required to make Vite accessible over network
+    port: 5173       // optional; ensure it matches your security group
+  }
 })
